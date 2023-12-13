4c31aaed97031d6cb7f847ef5b6516bcc644df68
jxy <jxy>
2023-12-13 09:06:02 +0000

    update

--- others/generate_large_scale_synth_regdata.py
+++ others/generate_large_scale_synth_regdata.py
@@ -3,8 +3,8 @@ sys.path.append('/Users/jiangxiaoyu/Desktop/All Projects/GPLVM_project_code/')
 from util_functions import *
 
 # generate DATA 
-w_n_C_total = 50 # totally 700 points for C
-w_n_outputs = 20 # 1500
+w_n_C_total = 20 # totally 700 points for C
+w_n_outputs = 2500 # 1500
 w_X_true, w_C_total, w_sample_total_data, kernel_parameters = tidily_sythetic_data_from_MOGP(n_C=w_n_C_total, n_X=w_n_outputs)
 
 # store DATA
